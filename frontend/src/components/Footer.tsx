98299e3bff6060c81ce6222d995b2e6daf0669c1
Bhargav sonani <149169635+bhargavsonani@users.noreply.github.com>
2026-02-25 21:03:20 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,23 +1,14 @@
-// import React from 'react'
-
-// const Footer = () => {
-//   return (
-//     <div className="text-center py-4 text-gray-400 text-sm border-t border-gray-800 mt-24">
-//       <div className="max-w-7xl mx-auto">
-//         <p className="text-center text-sm">© 2025 Site Builder. All rights reserved.</p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Footer
+import React from 'react'
 
 const Footer = () => {
   return (
     <div className="text-center py-4 text-gray-400 text-sm border-t border-gray-800 mt-24">
-      © 2025 Site Builder. All rights reserved.
+      <div className="max-w-7xl mx-auto">
+        <p className="text-center text-sm">© 2025 Site Builder. All rights reserved.</p>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
+
