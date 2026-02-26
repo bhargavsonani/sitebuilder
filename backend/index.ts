2d2c83abf252b0ac222e22f6bfe021cd1f378dae
Bhargav sonani <149169635+bhargavsonani@users.noreply.github.com>
2026-02-26 06:57:35 +0530

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://sitebuilder-rouge.vercel.app"],
   credentials: true,
 };
 
