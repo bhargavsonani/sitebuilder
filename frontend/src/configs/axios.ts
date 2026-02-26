fc8ad7857fbd42dcb35a0f2b3372eeacf5a5d9aa
Bhargav sonani <149169635+bhargavsonani@users.noreply.github.com>
2026-02-26 11:47:11 +0530

    Update axios.ts

--- frontend/src/configs/axios.ts
+++ frontend/src/configs/axios.ts
@@ -14,7 +14,7 @@
 import axios from "axios";
 
 const baseURL =
-  import.meta.env.VITE_BASEURL || "http://localhost:3000";
+  "https://sitebuilder-six.vercel.app" || "http://localhost:3000";
 
 const api = axios.create({
   baseURL,
